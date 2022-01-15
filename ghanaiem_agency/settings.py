53c48109055c662634c07eeec6ba4c7b78c20a9a
Aimene BAHRI <bahri.aimen48@gmail.com>
2022-01-16 00:18:44 +0100

    update staic dir file

--- ghanaiem_agency/settings.py
+++ ghanaiem_agency/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
